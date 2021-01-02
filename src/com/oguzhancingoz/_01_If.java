5bd61a4f21b420ddf414fb52aeb3f9b9c3b81c6a
oguzhan-cingoz <oguzhancingoz@yandex.com>
2021-01-03 00:17:50 +0300

    if yapısı ilk ders

---
+++ src/com/oguzhancingoz/_01_If.java
@@ -0,0 +1,18 @@
+package com.oguzhancingoz;
+
+public class _01_If {
+
+	public static void main(String[] args) {
+		
+		int puan = 99;
+		char harf = '-';
+		
+		if(puan>=90) {
+			harf = 'A';
+			System.out.println("Harf Notunuz =" + harf);
+			
+		}
+
+	}
+
+}
